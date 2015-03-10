5f0e0d1583bda6e48fc6de4bd0c0816ac68b72d5
Eric Ivander Jeadi <eric.thunderstorm@gmail.com>
2015-03-10 21:47:45 +0700

    Update server, login first to use chat

--- Server/server.c
+++ Server/server.c
@@ -11,8 +11,6 @@
 #define BACKLOG 10
 #define PRINT(msg) printf ("%s\n", msg)
 
-
-
 typedef struct user
 {
 	int sockcli;
@@ -25,6 +23,7 @@ typedef struct user
 user *first, *tmp;
 
 int readresponse(int, char *);
+
 pthread_t pt[100];
 int currentUser = 0;
 user clientList[100];
@@ -32,10 +31,10 @@ char buf[1000], respbuf[1000];
 
 void* threadClient(void *arg)
 {
-	int retval;
+	int retval = 0;
 	user *client = (user *) arg;
 	PRINT("Accept !");
-	sprintf(respbuf, "Selamat Datang\r\n");
+	sprintf(respbuf, "200#Connect Success\r\n");
 	retval = send(client->sockcli, respbuf, strlen(respbuf), 0);
 	while (retval != -1)
 	{
@@ -45,10 +44,10 @@ void* threadClient(void *arg)
 			break;
 		else if (strcasecmp(buf, "QUIT") == 0)
 		{
-			sprintf(respbuf, "221 Terminating program...\r\n");
+			sprintf(respbuf, "201#Logging Out\r\n");
 			retval = send(client->sockcli, respbuf, strlen(respbuf), 0);
 			retval = -1;
-			 
+			
 			if (*(client->first) == client)
 			    *(client->first) = client->next;
 			if (client->next != NULL)
@@ -56,37 +55,56 @@ void* threadClient(void *arg)
 			if (client->prev != NULL)
 				client->prev->next = client->next;
 		}
-		else if (strncasecmp(buf, "NAME", 4) == 0)
+		else if (strcmp(client->name, "Unknown") == 0 && strncasecmp(buf, "NAME", 4) == 0)
 		{
-			sscanf(buf,"%*s %s",client->name);
-			//PRINT(client->name);
+			int taken = 0;
+			char name[200];
+			sscanf(buf, "%*s %s", name);
+			PRINT(name);
+			user *tmp = *(client->first);
+			while (tmp != NULL)
+			{
+				if (strcmp(tmp->name, name) == 0)
+				{
+					sprintf(respbuf, "102#Username Already Taken\r\n");
+					taken = 1;
+					send(client->sockcli, respbuf, strlen(respbuf), 0);
+					retval = -1;
+					break;
+				}
+				tmp = tmp->next;
+			}
+			if (taken == 0)
+			{
+				sscanf(name, "%s", client->name);
+				sprintf(respbuf, "100#Username Set\r\n");
+				retval = send(client->sockcli, respbuf, strlen(respbuf), 0);
+			}
 		}
-		else if (strcasecmp(buf, "LIST") == 0)
+		else if (strcmp(client->name, "Unknown") != 0 && strcasecmp(buf, "LIST") == 0)
 		{
-			user *tmp = client;
-			while (tmp->prev != NULL)
-				tmp = tmp->prev;
+			user *tmp = *(client->first);
 			char text[1000];
 			memset(text, 0, sizeof(text));
-			while (tmp->next != NULL)
+			while (tmp != NULL)
 			{
-				strcat(text, tmp->name);
-				strcat(text, ";");
-				tmp = tmp->next;	
+				if(strcmp(tmp->name, "Unknown") != 0)
+				{
+					strcat(text, tmp->name);
+					strcat(text, ";");
+				}
+				tmp = tmp->next;
 			}
-			strcat(text, tmp->name);
-			strcat(text, ";\r\n");
+			strcat(text, "\r\n");
 			retval = send(client->sockcli, text, strlen(text), 0);
 		}
-		else if (strncasecmp(buf, "RCPT", 4) == 0)
+		else if (strcmp(client->name, "Unknown") != 0 && strncasecmp(buf, "RCPT", 4) == 0)
 		{
 			sscanf(buf, "%*s %s", client->dest);
 		}
-		else if (strncasecmp(buf, "MSG", 3) == 0)
+		else if (strcmp(client->name, "Unknown") != 0 && strncasecmp(buf, "MSG", 3) == 0)
 		{
-			user *tmp = client;
-			while (tmp->prev != NULL)
-				tmp = tmp->prev;
+			user *tmp = *(client->first);
 			char text[1000];
 			memset(text, 0, sizeof(text));
 			while (tmp->next != NULL)
@@ -105,7 +123,17 @@ void* threadClient(void *arg)
 				strcat(text, "\r\n");
 				retval = send(tmp->sockcli, text, strlen(text), 0);
 			}
-		}	
+		}
+		else if (strcmp(client->name, "Unknown") == 0) 
+		{
+			sprintf(respbuf, "101#Username Not Set\r\n");
+			retval = send(client->sockcli, respbuf, strlen(respbuf), 0);
+		}
+		else
+		{
+			sprintf(respbuf, "500#Unknown Command\r\n");
+			retval = send(client->sockcli, respbuf, strlen(respbuf), 0);
+		}
 	}
 	(*client->countUser)--;
 	close(client->sockcli);
@@ -137,6 +165,7 @@ int main ()
 	{
 		tmp = (user *)malloc(sizeof(user));
 		tmp->sockcli = accept(sockfd, (struct sockaddr *)&cliaddr, &clisize);
+		strcpy(tmp->name, "Unknown");
 		tmp->countUser = &currentUser;
 		tmp->first = &first;
 		tmp->prev = NULL;
@@ -157,7 +186,7 @@ int readresponse(int sockfd, char *buf)
 
 	do
 	{
-		retval = recv(sockfd, &tmp,1,0);
+		retval = recv(sockfd, &tmp, 1, 0);
 		if (retval > 0)
 		{
 			if (tmp == '\r')
